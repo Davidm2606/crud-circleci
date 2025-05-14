0fa07cb761dd37cf8e768cc30dfc449ba157f17f
Davidm2606 <davidteran2606@hotmail.com>
2025-05-14 15:55:26 -0500

    Primer commit - CRUD SQLite

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, redirect
+from app import app, db
+from app.models import User
+
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    name = request.form['name']
+    email = request.form['email']
+    user = User(name=name, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return redirect('/')
